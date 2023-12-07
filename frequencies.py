fce7a328355619be41786bcd193a9af390b54a7a
Isabella Cowin <isabella.cowin@kcl.ac.uk>
2023-12-07 18:24:14 +0000

    Add code

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if type(item) != str:
+            toString = str(item)
+            if (toString in frequencies):
+                frequencies[toString] +=1
+            else:
+                frequencies[toString] = 1
+        else: 
+            if (item in frequencies):
+                frequencies[item] +=1
+            else:
+                frequencies[item] = 1
     return frequencies
